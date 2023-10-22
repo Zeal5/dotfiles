f4113c8ade8d5276ba9d4c8b059153a984f884a6
Zeal5 <tassadiqshakeel5@gmail.com>
2023-10-23 02:52:39 +0500

    update keymaps/options

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,3 +6,16 @@ vim.g.maplocalleader = " "
 map("n", "<leader>l", "<cmd>Lazy<CR>", { desc = "Lazy" })
 map("i", "jj", "<ESC>", { desc = "Switch to normal mode" })
 
+-- copy to system clipboard
+map("v", "<leader>cp", [["+y]], { desc = "copy selected text" })
+map("n", "<leader>cp", [["+Y]], { desc = "copy line" })
+
+-- paste from system clipboard
+map({ "n", "v" }, "<leader>cv", [["+p]])
+
+
+
+
+
+
+
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,6 @@
-
-
-
 -- enable numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.number = true                           -- set numbered lines
+vim.opt.relativenumber = true                   -- set relative numbered lines
+vim.opt.shiftwidth = 2                          -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2                             -- insert 2 spaces for a tab
+
